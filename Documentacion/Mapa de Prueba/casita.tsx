dcb30f17776e50e27c6ec0c3fa2b8d26b58a0b08
Mapach3 <londmt3@gmail.com>
2017-10-20 18:29:37 -0300

    Agregado Mapa de Prueba
    Agregado Mapa de prueba

---
+++ Documentacion/Mapa de Prueba/casita.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="casa1" tilewidth="32" tileheight="32" tilecount="42" columns="6">
+ <image source="tilesets/casa1.png" trans="ffffff" width="200" height="250"/>
+</tileset>
