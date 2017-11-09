5e17b007a1b6055fa4ec1ac2ab68ef1b478c5f81
Tinchoob <martinbombara97@gmail.com>
2017-11-09 01:23:15 -0300

    Enemigos mapa 1
    No estan todos faltan algunos

---
+++ Codigo/EnemigosInicial.py
@@ -0,0 +1,25 @@
+import pygame
+import Enemigo
+
+pygame.init()
+
+enemigo1 = Enemigo("Enemigo 1 ",1529,556)
+enemigo2 = Enemigo("Enemigo 1 ",1400,750)
+enemigo3 = Enemigo("Enemigo 1 ",1700,900)
+enemigo4 = Enemigo("Enemigo 1 ",1750,1100)
+enemigo5 = Enemigo("Enemigo 1 ",1500,1200)
+enemigo6 = Enemigo("Enemigo 1 ",2270,800)
+enemigo7 = Enemigo("Enemigo 1 ",2270,900)
+enemigo8 = Enemigo("Enemigo 1 ",2250,1100)
+enemigo9 = Enemigo("Enemigo 1 ",2260,1200)
+enemigo10 = Enemigo("Enemigo 1 ",2000,1500)
+enemigo11 = Enemigo("Enemigo 2 ",2700,1450)
+enemigo12 = Enemigo("Enemigo 2 ",3050,700)
+enemigo13 = Enemigo("Enemigo 2 ",3240,1245)
+enemigo14 = Enemigo("Enemigo 2 ",3100,870)
+enemigo15 = Enemigo("Enemigo 2 ",3240,1123)
+enemigo16 = Enemigo("Enemigo 2 ",3180,1335)
+
+listaEnemigosInicial = [enemigo1,enemigo2,enemigo3,enemigo4,enemigo5,enemigo6,enemigo7,enemigo8,
+                        enemigo9,enemigo10,enemigo11,enemigo12,enemigo13,
+                        enemigo14,enemigo15,enemigo16]
