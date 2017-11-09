e08a8b1436598db5eb75a4e734ca599bf091ec71
iaastefano <31312613+iaastefano@users.noreply.github.com>
2017-11-09 01:02:19 -0300

    Revert "MapaHielo.py con enemigos"
    This reverts commit 0c3896869759a28b04b56c0e78534f0456cf2971.

--- Codigo/NivelHielo.py
+++ Codigo/NivelHielo.py
@@ -7,26 +7,13 @@ pygame.display.set_mode((800,600))
 
 ###enemigo = Enemigo(tipo,posx,posy)
 ### tipos: Enemigo 1, Enemigo 2, Mini Jefe, Jefe
-# HACER 14 ENEMIGOS
-enemigo1 = Enemigo("Enemigo 1",2809,1300)
-enemigo2 = Enemigo("Enemigo 2",2773,1051)
-enemigo3 = Enemigo("Enemigo 1",2696,870)
-enemigo4 = Enemigo("Enemigo 2",2671,1296)
-enemigo5 = Enemigo("Enemigo 1",2487,870)
-enemigo6 = Enemigo("Enemigo 1",2506,1038)
-enemigo7 = Enemigo("Enemigo 2",2537,1374)
-enemigo8 = Enemigo("Enemigo 2",2352,1215)
-enemigo9 = Enemigo("Enemigo 1",2257,1100)
-enemigo10 = Enemigo("Enemigo 2",1938,1337)
-enemigo11 = Enemigo("Enemigo 1",1490,1040)
-enemigo12 = Enemigo("Enemigo 2",1463,1275)
-enemigo13 = Enemigo("Enemigo 1",1158,1311)
-enemigo14 = Enemigo("Enemigo 2",1219,935)
+#enemigo1 = Enemigo("Enemigo 1", 800, 600)
+#enemigo2 = Enemigo("Enemigo 2", 1000, 600)
 
 ###pared = pygame.Rect(left,top,alto,ancho)
 
-listaEnemigosHielo = [enemigo1,enemigo2,enemigo3,enemigo4,enemigo5,enemigo6,enemigo7,enemigo8,enemigo9,enemigo10,enemigo11,enemigo12,enemigo13,enemigo14]
+#listaEnemigosHielo = [enemigo1,enemigo2]
 
 #listaWallsHielo = [pared1,pared2]
 
-#fondoHielo = pygame.image.load("MapaHieloFinal.png")            
+#fondoHielo = pygame.image.load(".png")            
