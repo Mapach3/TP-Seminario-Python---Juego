b675db4b874dc1d47de246b1ebf69dfa6628c5ef
Tinchoob <martinbombara97@gmail.com>
2017-11-09 14:28:33 -0300

    Agrego menu
    Agregue el menu al main

--- Codigo/Main.py
+++ Codigo/Main.py
@@ -11,6 +11,37 @@ from NivelJefeFinal import *
 from EnemigosInicial import *
 from NivelHielo import *
 
+
+class Boton(pygame.sprite.Sprite):
+    def __init__(self,imagen1,imagen2,x,y):  
+        self.imagen=imagen1
+        self.imagennormal=imagen1
+        self.imagenseleccion=imagen2
+        self.rect= imagen1.get_rect()
+        self.rect.left=x
+        self.rect.top=y
+        
+    def setImage(self,imagen):
+        self.imagen=imagen
+    def pintar(self,surface,cursor):
+        
+        if cursor.colliderect(self.rect): 
+            self.setImage(self.imagenseleccion)
+            
+        else:
+            self.setImage(self.imagennormal)
+        surface.blit(self.imagen,(self.rect.left,self.rect.top))  
+        
+        
+class cursor(pygame.Rect):
+    def __init__(self):
+        (self.x,self.y)=pygame.mouse.get_pos()
+        pygame.Rect.__init__(self,self.x,self.y,0,0)
+    def updatecursor(self):
+        (self.left,self.top)=pygame.mouse.get_pos()
+
+
+
 def moverCosasPantalla(personaje,fondo,pantalla,vx,vy,t,suceso,listaFlechas,listaWalls,listaEnemigos,informacion):
     if personaje.esta_furiozo:
         vx = vx*3
@@ -52,7 +83,7 @@ listaWalls=[]
 listaEnemigos=[]
 fondo = Fondo(pygame.image.load("Mapa1Final.png"),0,0)
 
-def main():
+def main(cargar=False):
     validaciongameover=False
     import pygame
     suceso = "no atacando"
@@ -163,6 +194,123 @@ def main():
         pygame.display.update()
     pygame.quit()
     
+def menu():
+    import pygame
+    pygame.init()
+    
+    pygame.mixer.music.load("Sonidos/menu.mid")
+    pantalla=pygame.display.set_mode((900,700))
+    pygame.display.set_caption("Vengeance")
+    relojmenu = pygame.time.Clock()
+    titulo=pygame.image.load("Vengeance/Titulo.png").convert_alpha()
+    descripcion=pygame.image.load("Vengeance/descripcion.png").convert_alpha()
+    pygame.mixer.music.play(30)
+    
+    newgame = Boton(pygame.image.load("Vengeance/jugar1.png").convert_alpha(),
+                    pygame.image.load("Vengeance/jugar2.png").convert_alpha(),280,290)
+    
+    controles = Boton(pygame.image.load("Vengeance/controles.png").convert_alpha(),
+                     pygame.image.load("Vengeance/controles2.png").convert_alpha(),280,370) # 280 para igualar chaboncito
+    
+    salir = Boton(pygame.image.load("Vengeance/salir.png").convert_alpha(),
+                  pygame.image.load("Vengeance/salir1.png").convert_alpha(),280,460)      #USAR ESTAS POS PARA LOS OTROS BOTONES
+    
+    
+    fuente1=pygame.font.SysFont("Arial", 30, False, False)
+    
+    selec1=pygame.mixer.Sound("selec.wav")  
+    
+    cont=False
+    
+    pygame.mixer.stop()
+    
+    
+    c1=cursor()
+    loadgamebool=None
+    
+    
+    
+    #Loop Principal!
+    while True:
+        
+        
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                pygame.quit()           # RETURN NONE PARA SALIR
+                return None
+            if event.type==pygame.MOUSEBUTTONDOWN:
+                if c1.colliderect(newgame.rect):
+                    selec1.play()
+                    pygame.quit()
+                    return False
+                elif c1.colliderect(controles.rect):
+                    selec1.play()
+                    cont=True
+                elif c1.colliderect(salir.rect):
+                    selec1.play()
+                    pygame.quit()
+                    return None
+                
+                
+                
+                
+        pantalla.fill((0,0,0))
+        relojmenu.tick(15)
+        pantalla.blit(titulo,(50,-20)) #Posicion del titulo del juego
+        pantalla.blit(descripcion,(170,660))  #Descripcion que va abajo de las opciones
+        #TEXTOS PARA CONTROLES
+        movim=fuente1.render("-Movimiento: Flechas Direccionales",0,(255,255,255))
+        atack1=fuente1.render("-Ataque 1: A",0,(255,255,255))
+        atack2=fuente1.render("-Ataque 2: D",0,(255,255,255))
+        atack3=fuente1.render("-Ataque 3: F",0,(255,255,255))
+        paus= fuente1.render("-Pulsa P para pausar el juego",0,(255,255,255))
+        newgame.pintar(pantalla, c1)
+        if cont==False:
+            controles.pintar(pantalla, c1)
+        salir.pintar(pantalla,c1)
+        
+        c1.updatecursor()
+        
+        if cont==True:
+            pantalla.blit(movim,(230,310))
+            pantalla.blit(atack1,(230,350))
+            pantalla.blit(atack2,(230,390))
+            pantalla.blit(atack3,(230,430))
+            pantalla.blit(paus,(230,470))
+        
+            
+    
+        
+        pygame.display.update()
+                    
+    
+#FUNCION START QUE CONTROLA LOS PASOS DE PANTALLA
+        
+        
+def start(loadbool=None):
+    if loadbool==None:
+        loadbool=menu()
+    if not(loadbool==None):
+        a=main(loadbool)
+        if a==True:
+            start(True)
+            
+            
+
+def pausa(pantalla,estaPausado):
+    import pygame
+    fondopausa = pygame.image.load("Vengeance/menupausa.png")
+    while estaPausado == True:
+        pantalla.blit(fondopausa,(0,0))
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_p:
+                        estaPausado=False
+        pygame.display.update()            
+    return                
+        
+     
+    
 def nivelInicial(listaWalls,listaEnemigos,fondo):
     listaWalls = listaWallsInicial
     listaEnemigos = listaEnemigosInicial
@@ -178,4 +326,4 @@ def nivelJefeFinal(listaWalls,listaEnemigos,fondo):
     listaEnemigos = []
     fondo = Fondo(fondoJefeFinal,0,0)
     
-main()
+start()
