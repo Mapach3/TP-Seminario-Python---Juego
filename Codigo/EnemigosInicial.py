f14c5bb08e339024e006d93a34f84feba862abba
Tinchoob <martinbombara97@gmail.com>
2017-11-09 01:40:38 -0300

    Enemigos nivel 1
    Completados

--- Codigo/EnemigosInicial.py
+++ Codigo/EnemigosInicial.py
@@ -3,23 +3,41 @@ import Enemigo
 
 pygame.init()
 
-enemigo1 = Enemigo("Enemigo 1 ",1529,556)
-enemigo2 = Enemigo("Enemigo 1 ",1400,750)
-enemigo3 = Enemigo("Enemigo 1 ",1700,900)
-enemigo4 = Enemigo("Enemigo 1 ",1750,1100)
-enemigo5 = Enemigo("Enemigo 1 ",1500,1200)
-enemigo6 = Enemigo("Enemigo 1 ",2270,800)
-enemigo7 = Enemigo("Enemigo 1 ",2270,900)
-enemigo8 = Enemigo("Enemigo 1 ",2250,1100)
-enemigo9 = Enemigo("Enemigo 1 ",2260,1200)
-enemigo10 = Enemigo("Enemigo 1 ",2000,1500)
-enemigo11 = Enemigo("Enemigo 2 ",2700,1450)
-enemigo12 = Enemigo("Enemigo 2 ",3050,700)
-enemigo13 = Enemigo("Enemigo 2 ",3240,1245)
-enemigo14 = Enemigo("Enemigo 2 ",3100,870)
-enemigo15 = Enemigo("Enemigo 2 ",3240,1123)
-enemigo16 = Enemigo("Enemigo 2 ",3180,1335)
+enemigo1 = Enemigo("Enemigo 1",1529,556)
+enemigo2 = Enemigo("Enemigo 1",1400,750)
+enemigo3 = Enemigo("Enemigo 1",1700,900)
+enemigo4 = Enemigo("Enemigo 1",1750,1100)
+enemigo5 = Enemigo("Enemigo 1",1500,1200)
+enemigo6 = Enemigo("Enemigo 1",2270,800)
+enemigo7 = Enemigo("Enemigo 1",2270,900)
+enemigo8 = Enemigo("Enemigo 1",2250,1100)
+enemigo9 = Enemigo("Enemigo 1",2260,1200)
+enemigo10 = Enemigo("Enemigo 1",2000,1500)
+enemigo11 = Enemigo("Enemigo 2",2700,1450)
+enemigo12 = Enemigo("Enemigo 2",3050,700)
+enemigo13 = Enemigo("Enemigo 2",3240,1245)
+enemigo14 = Enemigo("Enemigo 2",3100,870)
+enemigo15 = Enemigo("Enemigo 2",3240,1123)
+enemigo16 = Enemigo("Enemigo 2",3180,1335)
+enemigo17 = Enemigo("Enemigo 2",3350,1100)
+enemigo18 = Enemigo("Enemigo 2",3550,650)
+enemigo19 = Enemigo("Enemigo 2",3500,800)
+enemigo20 = Enemigo("Enemigo 2",3600,1000)
+enemigo21 = Enemigo("Enemigo 2",4350,540)
+enemigo22 = Enemigo("Enemigo 2",3900,800)
+enemigo23 = Enemigo("Enemigo 2",3940,940)
+enemigo24 = Enemigo("Enemigo 2",4250,900)
+enemigo25 = Enemigo("Enemigo 2",4500,900)
+enemigo26 = Enemigo("Enemigo 2",4270,1230)
+enemigo27 = Enemigo("Enemigo 2",4200,1450)
+enemigo28 = Enemigo("Enemigo 2",4500,1200)
+enemigo29 = Enemigo("Enemigo 2",4600,1370)
+enemigo30 = Enemigo("Enemigo 2",5300,900)
+enemigo31 = Enemigo("Enemigo 1",5300,1100)
 
 listaEnemigosInicial = [enemigo1,enemigo2,enemigo3,enemigo4,enemigo5,enemigo6,enemigo7,enemigo8,
                         enemigo9,enemigo10,enemigo11,enemigo12,enemigo13,
-                        enemigo14,enemigo15,enemigo16]
+                        enemigo14,enemigo15,enemigo16,enemigo17,enemigo18,
+                        enemigo19,enemigo20,enemigo21,enemigo22,enemigo23,
+                        enemigo24,enemigo25,enemigo26,enemigo27,enemigo28,
+                        enemigo29,enemigo30,enemigo31]
